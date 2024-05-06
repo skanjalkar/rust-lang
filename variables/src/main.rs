61c859b54113f14d4a2f2bae141b7227bf9a7472
skanjalkar <shreyaskanjalkar@gmail.com>
2024-05-05 20:03:35 -0400

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+
+
+fn main() {
+    let x = 5;
+    println!("The value of x is: {x}");
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in inner scope is {x}");
+    }
+    println!("The value of x is: {x}");
+}
